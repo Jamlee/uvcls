bceb07999d284ebaadfcaabc24964da5e59396b8
jamlee <1575103047@qq.com>
2022-07-14 00:07:12 +0800

    修复部分 tcp

--- src/lib/tcp.hpp
+++ src/lib/tcp.hpp
@@ -10,19 +10,143 @@
 
 #include "request.hpp"
 #include "stream.hpp"
+#include "handle.hpp"
+
 
 namespace uvcls {
+
+struct IPv6 {};
+struct IPv4 {};
+
+struct Addr {
+    std::string ip;    /*!< Either an IPv4 or an IPv6. */
+    unsigned int port; /*!< A valid service identifier. */
+};
+
+template<typename E>
+class Flags final {
+    static_assert(std::is_enum_v<E>);
+
+    using InnerType = std::underlying_type_t<E>;
+
+    constexpr InnerType toInnerType(E flag) const noexcept {
+        return static_cast<InnerType>(flag);
+    }
+
+public:
+    using Type = InnerType;
+
+    /**
+     * @brief Utility factory method to pack a set of values all at once.
+     * @return A valid instance of Flags instantiated from values `V`.
+     */
+    template<E... V>
+    static constexpr Flags<E> from() {
+        return (Flags<E>{} | ... | V);
+    }
+
+    /**
+     * @brief Constructs a Flags object from a value of the enum `E`.
+     * @param flag A value of the enum `E`.
+     */
+    constexpr Flags(E flag) noexcept
+        : flags{toInnerType(flag)} {}
+
+    /**
+     * @brief Constructs a Flags object from an instance of the underlying type
+     * of the enum `E`.
+     * @param f An instance of the underlying type of the enum `E`.
+     */
+    constexpr Flags(Type f)
+        : flags{f} {}
+
+    /**
+     * @brief Constructs an uninitialized Flags object.
+     */
+    constexpr Flags()
+        : flags{} {}
+
+    constexpr Flags(const Flags &f) noexcept
+        : flags{f.flags} {}
+
+    constexpr Flags(Flags &&f) noexcept
+        : flags{std::move(f.flags)} {}
+
+    constexpr Flags &operator=(const Flags &f) noexcept {
+        flags = f.flags;
+        return *this;
+    }
+
+    constexpr Flags &operator=(Flags &&f) noexcept {
+        flags = std::move(f.flags);
+        return *this;
+    }
+
+    /**
+     * @brief Or operator.
+     * @param f A valid instance of Flags.
+     * @return This instance _or-ed_ with `f`.
+     */
+    constexpr Flags operator|(const Flags &f) const noexcept {
+        return Flags{flags | f.flags};
+    }
+
+    /**
+     * @brief Or operator.
+     * @param flag A value of the enum `E`.
+     * @return This instance _or-ed_ with `flag`.
+     */
+    constexpr Flags operator|(E flag) const noexcept {
+        return Flags{flags | toInnerType(flag)};
+    }
+
+    /**
+     * @brief And operator.
+     * @param f A valid instance of Flags.
+     * @return This instance _and-ed_ with `f`.
+     */
+    constexpr Flags operator&(const Flags &f) const noexcept {
+        return Flags{flags & f.flags};
+    }
+
+    /**
+     * @brief And operator.
+     * @param flag A value of the enum `E`.
+     * @return This instance _and-ed_ with `flag`.
+     */
+    constexpr Flags operator&(E flag) const noexcept {
+        return Flags{flags & toInnerType(flag)};
+    }
+
+    /**
+     * @brief Checks if this instance is initialized.
+     * @return False if it's uninitialized, true otherwise.
+     */
+    explicit constexpr operator bool() const noexcept {
+        return !(flags == InnerType{});
+    }
+
+    /**
+     * @brief Casts the instance to the underlying type of `E`.
+     * @return An integral representation of the contained flags.
+     */
+    constexpr operator Type() const noexcept {
+        return flags;
+    }
+
+private:
+    InnerType flags;
+};
 
 enum class UVTCPFlags : std::underlying_type_t<uv_tcp_flags> {
     IPV6ONLY = UV_TCP_IPV6ONLY
 };
 
 class TCPHandle final: public StreamHandle<TCPHandle, uv_tcp_t> {
+
 public:
     using Time = std::chrono::duration<unsigned int>;
-    using Bind = UVTCPFlags;
-    using IPv4 = uvcls::IPv4;
-    using IPv6 = uvcls::IPv6;
+    using OSSocketHandle = uvcls::UVTypeWrapper<uv_os_sock_t>; 
 
     explicit TCPHandle(std::shared_ptr<Loop> ref, unsigned int f = {});
 
@@ -36,13 +160,13 @@ public:
 
     bool simultaneousAccepts(bool enable = true);
 
-    void bind(const sockaddr &addr, Flags<Bind> opts = Flags<Bind>{});
+    void bind(const sockaddr &addr, Flags<UVTCPFlags> opts = Flags<UVTCPFlags>{});
 
     template<typename I = IPv4>
-    void bind(const std::string &ip, unsigned int port, Flags<Bind> opts = Flags<Bind>{});
+    void bind(const std::string &ip, unsigned int port, Flags<UVTCPFlags> opts = Flags<UVTCPFlags>{});
 
     template<typename I = IPv4>
-    void bind(Addr addr, Flags<Bind> opts = Flags<Bind>{});
+    void bind(Addr addr, Flags<UVTCPFlags> opts = Flags<UVTCPFlags>{});
 
     template<typename I = IPv4>
     Addr sock() const noexcept;
@@ -66,35 +190,39 @@ private:
         FLAGS
     } tag;
 
+    // flags 配置用途，不是简简单单的 1 个 int
     unsigned int flags;
 };
 
-extern template void TCPHandle::bind<IPv4>(const std::string &, unsigned int, Flags<Bind>);
-extern template void TCPHandle::bind<IPv6>(const std::string &, unsigned int, Flags<Bind>);
+// 显式专用化(模板特化)声明在前。explicit instantiations
+template void TCPHandle::bind<IPv4>(const std::string &, unsigned int, Flags<UVTCPFlags>);
+template void TCPHandle::bind<IPv6>(const std::string &, unsigned int, Flags<UVTCPFlags>);
 
-extern template void TCPHandle::bind<IPv4>(Addr, Flags<Bind>);
-extern template void TCPHandle::bind<IPv6>(Addr, Flags<Bind>);
+template void TCPHandle::bind<IPv4>(Addr, Flags<UVTCPFlags>);
+template void TCPHandle::bind<IPv6>(Addr, Flags<UVTCPFlags>);
 
-extern template Addr TCPHandle::sock<IPv4>() const noexcept;
-extern template Addr TCPHandle::sock<IPv6>() const noexcept;
+template Addr TCPHandle::sock<IPv4>() const noexcept;
+template Addr TCPHandle::sock<IPv6>() const noexcept;
 
-extern template Addr TCPHandle::peer<IPv4>() const noexcept;
-extern template Addr TCPHandle::peer<IPv6>() const noexcept;
+template Addr TCPHandle::peer<IPv4>() const noexcept;
+template Addr TCPHandle::peer<IPv6>() const noexcept;
 
-extern template void TCPHandle::connect<IPv4>(const std::string &, unsigned int);
-extern template void TCPHandle::connect<IPv6>(const std::string &, unsigned int);
+template void TCPHandle::connect<IPv4>(const std::string &, unsigned int);
+template void TCPHandle::connect<IPv6>(const std::string &, unsigned int);
 
-extern template void TCPHandle::connect<IPv4>(Addr addr);
-extern template void TCPHandle::connect<IPv6>(Addr addr);
+template void TCPHandle::connect<IPv4>(Addr addr);
+template void TCPHandle::connect<IPv6>(Addr addr);
 
-UVCLS_INLINE TCPHandle::TCPHandle(ConstructorAccess ca, std::shared_ptr<Loop> ref, unsigned int f)
-    : StreamHandle{ca, std::move(ref)}, tag{f ? FLAGS : DEFAULT}, flags{f} {}
+
+// TCPHandle 构造函数。首先调用 StreamHandle{std::move(ref)} 然后 tag 和 flags
+UVCLS_INLINE TCPHandle::TCPHandle(std::shared_ptr<Loop> ref, unsigned int f)
+    : StreamHandle{std::move(ref)}, tag{f ? FLAGS : DEFAULT}, flags{f} {}
 
 UVCLS_INLINE bool TCPHandle::init() {
     return (tag == FLAGS) ? initialize(&uv_tcp_init_ex, flags) : initialize(&uv_tcp_init);
 }
 
-UVCLS_INLINE void TCPHandle::open(OSSocketHandle socket) {
+UVCLS_INLINE void TCPHandle::open(TCPHandle::OSSocketHandle socket) {
     invoke(&uv_tcp_open, get(), socket);
 }
 
@@ -110,19 +238,19 @@ UVCLS_INLINE bool TCPHandle::simultaneousAccepts(bool enable) {
     return (0 == uv_tcp_simultaneous_accepts(get(), enable));
 }
 
-UVCLS_INLINE void TCPHandle::bind(const sockaddr &addr, Flags<Bind> opts) {
+UVCLS_INLINE void TCPHandle::bind(const sockaddr &addr, Flags<UVTCPFlags> opts) {
     invoke(&uv_tcp_bind, get(), &addr, opts);
 }
 
 template<typename I>
-UVCLS_INLINE void TCPHandle::bind(const std::string &ip, unsigned int port, Flags<Bind> opts) {
+UVCLS_INLINE void TCPHandle::bind(const std::string &ip, unsigned int port, Flags<UVTCPFlags> opts) {
     typename details::IpTraits<I>::Type addr;
     details::IpTraits<I>::addrFunc(ip.data(), port, &addr);
     bind(reinterpret_cast<const sockaddr &>(addr), std::move(opts));
 }
 
 template<typename I>
-UVCLS_INLINE void TCPHandle::bind(Addr addr, Flags<Bind> opts) {
+UVCLS_INLINE void TCPHandle::bind(Addr addr, Flags<UVTCPFlags> opts) {
     bind<I>(std::move(addr.ip), addr.port, std::move(opts));
 }
 
@@ -162,25 +290,6 @@ UVCLS_INLINE void TCPHandle::connect(const sockaddr &addr) {
 UVCLS_INLINE void TCPHandle::closeReset() {
     invoke(&uv_tcp_close_reset, get(), &this->closeCallback);
 }
-
-// explicit instantiations
-template void TCPHandle::bind<IPv4>(const std::string &, unsigned int, Flags<Bind>);
-template void TCPHandle::bind<IPv6>(const std::string &, unsigned int, Flags<Bind>);
-
-template void TCPHandle::bind<IPv4>(Addr, Flags<Bind>);
-template void TCPHandle::bind<IPv6>(Addr, Flags<Bind>);
-
-template Addr TCPHandle::sock<IPv4>() const noexcept;
-template Addr TCPHandle::sock<IPv6>() const noexcept;
-
-template Addr TCPHandle::peer<IPv4>() const noexcept;
-template Addr TCPHandle::peer<IPv6>() const noexcept;
-
-template void TCPHandle::connect<IPv4>(const std::string &, unsigned int);
-template void TCPHandle::connect<IPv6>(const std::string &, unsigned int);
-
-template void TCPHandle::connect<IPv4>(Addr addr);
-template void TCPHandle::connect<IPv6>(Addr addr);
 
 }
 
